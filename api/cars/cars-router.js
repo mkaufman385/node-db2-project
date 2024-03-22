198c51075bc59af7c77c1928cb89e3703cb6366c
mkaufman385 <mkaufman385@gmail.com>
2024-03-22 16:03:31 -0400

    wip

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const Car = require("./cars-model");
-const { checkCarId } = require("./cars-middleware");
+const {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+} = require("./cars-middleware");
 
 const router = express.Router();
 
@@ -17,8 +22,14 @@ router.get("/:id", checkCarId, async (req, res, next) => {
   res.json(req.car);
 });
 
-router.post("/", async (req, res, next) => {
-  res.send("posting new car");
-});
+router.post(
+  "/",
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+  async (req, res, next) => {
+    res.send("posting new car");
+  }
+);
 
 module.exports = router;
