3de6888de3f0072aeb4fa63611cd3e9b9e3ab76b
mkaufman385 <mkaufman385@gmail.com>
2024-03-22 12:44:41 -0400

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,11 @@
 // STRETCH
+const cars = [
+  {
+    vin: "11111111111111111",
+    make: "totyota",
+    model: "prius",
+    mileage: 215000,
+    title: "clean",
+    transmission: "manual",
+  },
+];
