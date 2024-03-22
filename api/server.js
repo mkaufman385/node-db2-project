ad3a69c3882c581aa9bfb45f094ff488ee611009
mkaufman385 <mkaufman385@gmail.com>
2024-03-22 14:54:33 -0400

    wip

--- api/server.js
+++ api/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 
+const carsRouter = require("./cars/cars-router");
+
 const server = express();
 
 server.use(express.json());
+
+server.use("/api/cars", carsRouter); // why is this not working?
 
 server.use("*", (req, res, next) => {
   next({ status: 404, message: "not found from server.js!" });
